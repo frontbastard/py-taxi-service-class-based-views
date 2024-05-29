4eb39282d3d09266dfb8f7c8143eb4b95731dbc7
Pavlo <pavlo@MacBook-Pro-de-Pavlo.local>
2024-05-29 21:53:01 +0200

    Fix: PR remarks

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from .views import (index, CarListView, CarDetailView, DriverListView,
-                    DriverDetailView, ManufacturerListView)
+from .views import (
+    index,
+    CarListView,
+    CarDetailView,
+    DriverListView,
+    DriverDetailView,
+    ManufacturerListView
+)
 
 urlpatterns = [
     path("", index, name="index"),
